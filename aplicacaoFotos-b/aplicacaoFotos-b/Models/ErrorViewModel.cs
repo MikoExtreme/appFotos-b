7cbca2f055396133d9ee21cc5571f0a925f8f90f
MikoExtreme <160228078+MikoExtreme@users.noreply.github.com>
2025-03-05 09:21:23 +0000

    Bolas
    Bolas

---
+++ aplicacaoFotos-b/aplicacaoFotos-b/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aplicacaoFotos_b.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
